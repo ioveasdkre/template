b8815322036794c95007f1868d46ce832aaa627a
Benson <D000019611@cgu.edu.tw>
2025-08-28 16:51:15 +0800

    feat: 1.update eslint setting

--- eslint.config.js
+++ eslint.config.js
@@ -1,26 +1,35 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import { defineConfig } from 'eslint/config';
-import typescriptEslintPlugin from '@typescript-eslint/eslint-plugin';
-import typescriptEslintParser from '@typescript-eslint/parser';
-import prettierPlugin from 'eslint-plugin-prettier';
+// * 作法一 使用 eslint
+// import js from '@eslint/js';
+// import globals from 'globals';
+// import { defineConfig } from 'eslint/config';
+// import typescriptEslintPlugin from '@typescript-eslint/eslint-plugin';
+// import typescriptEslintParser from '@typescript-eslint/parser';
+// import prettierPlugin from 'eslint-plugin-prettier';
+// import configPrettier from 'eslint-config-prettier';
 
-export default defineConfig([
-  {
-    files: ['**/*.{js,mjs,cjs,ts}'],
-    ignores: ['src/**/*.test.ts', 'src/frontend/generated/*', '**/*.config.js'],
-    languageOptions: {
-      parser: typescriptEslintParser,
-      parserOptions: {
-        project: ['./tsconfig.json'],
-        sourceType: 'module',
-      },
-    },
-    plugins: { js, '@typescript-eslint': typescriptEslintPlugin, prettier: prettierPlugin },
-    extends: ['js/recommended'],
-    languageOptions: { globals: globals.browser },
-    rules: {
-      'prettier/prettier': 'error',
-    },
-  },
-]);
+// export default defineConfig([
+//   {
+//     files: ['**/*.{js,mjs,cjs,ts}'],
+//     ignores: ['src/**/*.test.ts', 'src/frontend/generated/*', '**/*.config.js'],
+//     languageOptions: {
+//       parser: typescriptEslintParser,
+//       parserOptions: {
+//         sourceType: 'module',
+//         project: ['./tsconfig.json'],
+//       },
+//     },
+//     plugins: { js, '@typescript-eslint': typescriptEslintPlugin, prettier: prettierPlugin },
+//     extends: ['js/recommended', configPrettier],
+//     languageOptions: { globals: globals.browser },
+//     rules: {
+//       'prettier/prettier': 'error',
+//     },
+//   },
+// ]);
+
+// * 作法二 使用 typescript-eslint
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-plugin-prettier/recommended';
+
+export default tseslint.config(eslint.configs.recommended, tseslint.configs.recommended, prettier);
